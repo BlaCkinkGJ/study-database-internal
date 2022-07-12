dede9d6ebccfd882fafb9c6668cac1fbfdf88cc5
BlaCkinkGJ <ss5kijun@gmail.com>
2022-07-13 02:08:16 +0900

    remove serde for serialization

--- btree/src/slot/cell.rs
+++ btree/src/slot/cell.rs
@@ -1,12 +1,10 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize)]
+#[repr(C)]
 pub struct Offset {
     payload_size: u64,
     start_cell_pos: u64,
 }
 
-#[derive(Serialize, Deserialize)]
+#[repr(C)]
 pub struct Cell {
     payload: Vec<u8>,
     next_cell_pos: u64,
